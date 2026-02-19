0d3a4057e35125795379ccef95771966d7b83fae
Md Nazmul Bijoy <nazmulbijoy9105@gmail.com>
2026-02-19 16:11:50 +0600

    fix: add NEXT_PUBLIC_API_URL to next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,11 @@
-// module.exports = {
-//   experimental: {
-//     turbo: false,
-//   },
-// };
-module.exports = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  // experimental: {
+  //   turbo: false,
+  // },
+};
+
+module.exports = nextConfig;
