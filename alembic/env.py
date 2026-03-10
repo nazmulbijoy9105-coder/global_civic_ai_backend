49366437526b0cc19dc04bfe927a5d7cc8f346b5
Md Nazmul Bijoy <nazmulbijoy9105@gmail.com>
2026-03-10 06:41:41 +0600

    Update Alembic config and add backend logic

--- alembic/env.py
+++ alembic/env.py
@@ -6,13 +6,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 config = context.config
-config.set_main_option("sqlalchemy.url", os.environ.get("DATABASE_URL", ""))
+config.set_main_option("sqlalchemy.url", os.environ.get("postgresql://civicuser:eC4wiydQALfMdj9YuRiVMp51n1M7eZCr@dpg-d6nf6ifgi27c7393q0u0-a.oregon-postgres.render.com/civic_ai_lmxt", ""))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-from backend.app.database import Base
-from backend.app import models  # ✅ import all models
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from app.database import Base
+from app import models  # ✅ ensures all models are registered
 
 target_metadata = Base.metadata
 
